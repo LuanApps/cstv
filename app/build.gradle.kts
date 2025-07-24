63706b9e5a40b02fd32d55ac6acb7495793c5500
LuanApps <luanpramos@gmail.com>
2025-07-24 12:14:16 -0300

    Update Coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,14 +66,14 @@ dependencies {
     /** Android Core*/
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
 
     /** Compose */
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
 
     /** testing */
     testImplementation(libs.junit)
