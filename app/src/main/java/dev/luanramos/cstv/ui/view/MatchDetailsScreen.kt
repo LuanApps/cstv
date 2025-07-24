f3564949f418dac66e3eb95ae80af8ade33c74ec
LuanApps <luanpramos@gmail.com>
2025-07-24 18:32:13 -0300

    Make MatchDetailsScreen vertically scrollable to support landscape mode

--- app/src/main/java/dev/luanramos/cstv/ui/view/MatchDetailsScreen.kt
+++ app/src/main/java/dev/luanramos/cstv/ui/view/MatchDetailsScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -51,6 +53,7 @@ fun MatchDetailsScreen(
     val team1Players by viewModel.team1Players.collectAsState()
     val team2Players by viewModel.team2Players.collectAsState()
     val isLoadingPlayers by viewModel.isLoadingPlayers.collectAsState()
+    val scrollState = rememberScrollState()
 
     val unknownString = stringResource(R.string.label_unknown)
 
@@ -88,7 +91,9 @@ fun MatchDetailsScreen(
                     CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
                 } else {
                     Column(
-                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                        modifier = Modifier
+                            .verticalScroll(scrollState),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
                     ) {
                         Spacer(modifier = Modifier.height(8.dp))
                         Row {
